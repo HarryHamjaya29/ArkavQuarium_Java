6d5380dc5fb2453a6c1db8a72ba5022ac9f6a34c
Tony <buddy90_lost@yahoo.co.id>
2018-04-24 22:56:01 +0700

    update junit ikan

--- src/IkanTest.java
+++ src/IkanTest.java
@@ -5,53 +5,71 @@ import static org.junit.Assert.*;
 public class IkanTest {
 
     @Test
-<<<<<<< HEAD
-=======
-    public void getBanyakikan() {
-        Ikan test = new Piranha(1,1,1,100);
-
-    }
-
-    @Test
->>>>>>> 729179d831946da3875e9d4d2e50c314010d80ca
     public void getLapar() {
-
+        Ikan test = new Piranha(30, 30, 3.14, 100);
+        assertFalse(test.getLapar());
     }
 
     @Test
     public void setLapar() {
+        Ikan test = new Piranha(30, 30, 3.14, 100);
+        assertFalse(test.getLapar());
+        test.setLapar(true);
+        assertTrue(test.getLapar());
     }
 
     @Test
     public void getTahanKenyang() {
+        Ikan test = new Piranha(30, 30, 3.14, 100);
+        assertEquals(9, test.getTahanKenyang());
     }
 
     @Test
     public void getWaktuMakan() {
+        Ikan test = new Piranha(30, 30, 3.14, 100);
+        assertEquals(0f, test.getWaktuMakan(), 1e-10);
     }
 
     @Test
     public void setWaktuMakan() {
+        Ikan test = new Piranha(30, 30, 3.14, 100);
+        assertEquals(0f, test.getWaktuMakan(), 1e-10);
+        test.setWaktuMakan(5.6f);
+        assertEquals(5.6f, test.getWaktuMakan(), 1e-10);
     }
 
     @Test
     public void getHunger() {
+        Ikan test = new Piranha(30, 30, 3.14, 100);
+        assertEquals(15, test.getHunger());
     }
 
     @Test
     public void getType() {
+        Ikan test = new Piranha(30, 30, 3.14, 100);
+        assertEquals("piranha", test.getType());
+        Ikan test2 = new Guppy(30, 30, 3.14, 100);
+        assertEquals("guppy", test.getType());
     }
 
     @Test
     public void setType() {
+        Ikan test = new Piranha(30, 30, 3.14, 100);
+        assertEquals("piranha", test.getType());
+        test.setType("dory");
+        assertEquals("dory", test.getType());
     }
 
     @Test
     public void getPointTujuan() {
+        Ikan test = new Piranha(30, 30, 3.14, 100);
+        Ikan test2 = new Piranha(30, 30, 3.14, 100);
+        assertNotEquals(test.getPointTujuan(), test2.getPointTujuan());
     }
 
     @Test
     public void setPointTujuan() {
+        //Posisi
     }
 
     @Test
